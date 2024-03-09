7cbd7f447882df4a58ed17c0cd85d8cfdc2cac84
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2024-03-10 02:11:51 +0900

    docs: update

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 ## Development
 
 ```sh
+rye config --set-bool behavior.use-uv=true # uv
 rye sync --no-lock # setup
 rye run main # main.py
 rye run test # pytest
--- src/main.py
+++ src/main.py
@@ -1,9 +1,6 @@
 import torch
 import utils
 
-print(torch.__version__)
-print(torch.cuda.is_available())
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(device)
-
+print(torch.__version__, device)
 print(utils.add(1, 2))
